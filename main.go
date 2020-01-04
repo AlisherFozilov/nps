4ebc533e07dff03b78bac2a42cb4863840d82d49
Alisher <sambusa001@mail.ru>
2020-01-04 12:24:32 +0500

    hi

--- main.go
+++ main.go
@@ -4,17 +4,12 @@ import "fmt"
 
 func main() {
 	score1 := 10
-	score2 := 10
-	score3 := 10
-
+	//score2 := 10
+	//score3 := 10
 	promoters := 0
 	detractorov := 0
-
 	if score1 >= 0 {
 		promoters = promoters + 1
-	}
-	if score1 <= 0 {
-		detractorov = detractorov + 1
 	}
 
 	nps := (promoters-detractorov) / 3*100
